068874b6581c0a4feafcce34430489dfa14f1386
Antonio-valenzuela <za230120184@zapopan.tecmm.edu.mx>
2026-04-15 19:26:51 -0600

    feat: rubricas tipo classroom en admin

--- backend/src/services/database.js
+++ backend/src/services/database.js
@@ -1,20 +1,32 @@
 ﻿require("dotenv").config();
 const mysql = require("mysql2");
 
+<<<<<<< HEAD
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
+=======
+const pool = mysql.createPool({
+  host: "database",
+  user: "root",
+  password: "root",
+  database: "sistematesis",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+>>>>>>> cc7e552 (feat: avances de Yahir)
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error("Error conectando a la base:", err);
     return;
   }
   console.log("Conectado a la base sistematesis");
+  connection.release();
 });
 
-module.exports = connection;
+module.exports = pool;
